6c9e08ca57eb61f23f1baadf8628eea1c2a76b4b
zentornoe <andycityfc@gmail.com>
2026-05-29 13:39:41 +0900

    tc01: not o'clock

--- Project11/test.cpp
+++ Project11/test.cpp
@@ -1,39 +1,52 @@
 #include "gmock/gmock.h"
 #include "booking_scheduler.cpp"
 
-TEST(BookingSchedulerTest, 예약은정시에만가능하다정시가아닌경우예약불가) {
+TEST(BookingSchedulerTest, t1) {//예약은_정시에만_가능하다_정시가_아닌경우_예약불가)
+	tm notOnTheHour = { 0 };
+	notOnTheHour.tm_year = 2021 - 1900;
+	notOnTheHour.tm_mon = 03 - 1;
+	notOnTheHour.tm_mday = 26;
+	notOnTheHour.tm_hour = 9;	// 09 h
+	notOnTheHour.tm_min = 05;	// 05 m
+	notOnTheHour.tm_isdst = -1;
+	mktime(&notOnTheHour);
+
+	Customer customer{ "Fake name", "010-1234-5678" };
+	Schedule* schedule = new Schedule{ notOnTheHour, 1, customer };
+	BookingScheduler bookingScheduler{ 3 };
+
+	EXPECT_THROW({ bookingScheduler.addSchedule(schedule); }, std::runtime_error);
+}
+
+TEST(BookingSchedulerTest, t2) {//예약은_정시에만_가능하다_정시인_경우_예약가능) {
 
 }
 
-TEST(BookingSchedulerTest, 예약은정시에만가능하다정시인경우예약가능) {
+TEST(BookingSchedulerTest, t3) {//시간대별_인원제한이_있다_같은_시간대에_Capacity_초과할_경우_예외발생) {
 
 }
 
-TEST(BookingSchedulerTest, 시간대별인원제한이있다같은시간대에Capacity초과할경우예외발생) {
+TEST(BookingSchedulerTest, t4) {//시간대별_인원제한이_있다_같은_시간대가_다르면_Capacity_차있어도_스케쥴_추가_성공) {
 
 }
 
-TEST(BookingSchedulerTest, 시간대별인원제한이있다같은시간대가다르면Capacity차있어도스케쥴추가성공) {
+TEST(BookingSchedulerTest, t5) {//예약완료시_SMS는_무조건_발송) {
 
 }
 
-TEST(BookingSchedulerTest, 예약완료시SMS는무조건발송) {
+TEST(BookingSchedulerTest, t6) {//이메일이_없는_경우에는_이메일_미발송) {
 
 }
 
-TEST(BookingSchedulerTest, 이메일이없는경우에는이메일미발송) {
+TEST(BookingSchedulerTest, t7) {//이메일이_있는_경우에는_이메일_발송) {
 
 }
 
-TEST(BookingSchedulerTest, 이메일이있는경우에는이메일발송) {
+TEST(BookingSchedulerTest, t8) {//현재날짜가_일요일인_경우_예약불가_예외처리) {
 
 }
 
-TEST(BookingSchedulerTest, 현재날짜가일요일인경우예약불가예외처리) {
-
-}
-
-TEST(BookingSchedulerTest, 현재날짜가일요일이아닌경우예약가능) {
+TEST(BookingSchedulerTest, t9) {//현재날짜가_일요일이_아닌경우_예약가능) {
 
 }
 
